8cd7e0459baea682951e22df68faf46a9b7aa385
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2023-04-28 17:53:10 -0300

    Refactor: home screen adjustment

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { AboutComponent } from './components/about/about.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { NavbarModule } from '../shared/navbar/navbar.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NavbarModule
   ],
   declarations: [
     HomeComponent,
